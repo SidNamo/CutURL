5a32e150638917953147608db77ba5847c6c1910
sidnamo <haheehong3@naver.com>
2025-05-30 21:17:07 +0900

    commit

--- callme.sh
+++ callme.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo "$(date) - callme.sh 시작"
-
-CURRENT_DOMAIN=${CURRENT_DOMAIN}
+echo "$(date) - callme.sh 시작- $CURRENT_DOMAIN"
 
 # curl 결과와 상태코드 모두 저장
 response=$(curl -s -w "\nHTTP_STATUS_CODE:%{http_code}\n" -X GET "$CURRENT_DOMAIN")
